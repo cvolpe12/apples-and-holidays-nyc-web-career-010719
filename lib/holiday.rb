ce783498a56e7e58ab5e0c08cfeec6c2f3f137a6
Christopher Volpe <christopher.volpe12@gmail.com>
2019-01-04 03:14:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
   end
   holiday_hash
 end
-    
+
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
